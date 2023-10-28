d5d8284e39949c51e91531c9e51e889ff6a7322e
Hari <122802667+Hari-webdev@users.noreply.github.com>
2023-10-28 09:15:59 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
-const ALCHEMY_API_KEY = "iuCGNrBkd_eb8d-9AsFTgKs57z6ZJ02R" ;
-const GOERLI_PRIVATE_KEY = "e06b8bac3c1403423806588142ce1de02987b9a4c94c53bcda0fa8802e88c19a";
+const ALCHEMY_API_KEY = "iuCGNrBkd_eb8d-9AsFTgKs....." ;
+const GOERLI_PRIVATE_KEY = "e06b.....";
 
 module.exports = {
   solidity: "0.8.17",
